c1c6fb6549bfbc532184be473fe730b60d90663e
unknown <patezno97@gmail.com>
2019-01-08 17:57:42 +0100

    added repr

--- item.py
+++ item.py
@@ -16,6 +16,9 @@ class Item(Updateable):
     def getQuality(self):
         return self.quality
 
+    def __repr__(self):
+        return '%s, %s, %s' % (self.name, self.sell_in, self.quality)
+
 
 if __name__ == '__main__':
 
